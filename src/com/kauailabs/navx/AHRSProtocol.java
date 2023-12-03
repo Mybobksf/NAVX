e5ff18604df9af3b9a80c3f7a818e1f6fafa7632
Anilkumar Sarvabatla <71952099+Mybobksf@users.noreply.github.com>
2023-12-03 13:44:45 -0800

    Update AHRSProtocol.java

--- src/com/kauailabs/navx/AHRSProtocol.java
+++ src/com/kauailabs/navx/AHRSProtocol.java
@@ -1,1 +1,12 @@
+package com.kauailabs.navx;
 
+
+public class AHRSProtocol extends IMUProtocol {
+  //variable initalization
+  public static final byte NAVX_CAL_STATUS_IMU_CAL_STATE_MASK = 0x03;
+	public static final byte NAVX_CAL_STATUS_IMU_CAL_INPROGRESS = 0x00;
+	public static final byte NAVX_CAL_STATUS_IMU_CAL_ACCUMULATE = 0x01;
+	public static final byte NAVX_CAL_STATUS_IMU_CAL_COMPLETE = 0x02;
+	public static final byte NAVX_CAL_STATUS_MAG_CAL_COMPLETE = 0x04;
+	public static final byte NAVX_CAL_STATUS_BARO_CAL_COMPLETE = 0x08;
+}
